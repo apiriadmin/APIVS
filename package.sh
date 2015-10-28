0b8e6b3111432c425d51ffc34a1f3bc4fc29d55f
grantga <grant.gardner@intelight-its.com>
2015-10-27 17:10:30 -0700

    Move C2* files to tests and update package.sh to not copy them by name

--- package.sh
+++ package.sh
@@ -10,8 +10,5 @@ cp bin/* usb_root/APIVS/bin/
 cp lib/* usb_root/APIVS/lib/
 cp tests/* usb_root/run/
 cp support/VS_config_1.txt usb_root/run/VS_config_1.txt
-cp support/C2001d_vd_00 usb_root/run/C2001d_vd_00
-cp support/C2001d_vd_clr usb_root/run/C2001d_vd_clr
-cp support/Cxxxx_key0.txt usb_root/run/Cxxxx_key0.txt
 cp support/startup usb_root/startup
 cp support/runAPIVS usb_root/runAPIVS
